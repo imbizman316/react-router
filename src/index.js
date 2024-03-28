aa1dbb2f2fb631db617b93b4ce0cf0456d522346
Mike Lee <pride81_@hotmail.com>
2024-03-28 19:29:26 +0900

    Add gitignore

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Router from './components/react_router_6/Router';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
